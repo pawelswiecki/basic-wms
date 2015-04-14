dcf4e5bcdadc3ebc954f6e1d860fa37acf90c867
Paweł Święcki <pawel.swiecki@gmail.com>
2015-04-14 12:19:48 +0000

    added property decorators in db model

--- database/db_setup.py
+++ database/db_setup.py
@@ -8,97 +8,182 @@ db = SQLAlchemy(app)
 class Warehouse(db.Model):
     __tablename__ = 'warehouse'
 
-    id_ = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    location = db.Column(db.String(120), nullable=False)
+    _id = db.Column(db.Integer, primary_key=True, nullable=False)
+    _name = db.Column(db.String(80), unique=True, nullable=False)
+    _location = db.Column(db.String(120), nullable=False)
     
     def __init__(self, name, location):
-        self.name = name
-        self.location = location
+        self._name = name
+        self._location = location
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return "<Warehouse #{}>".format(self.id_)
+        return "<Warehouse #{}>".format(self._id)
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def location(self):
+        return self._location
+    @location.setter
+    def location(self, value):
+        self._location = value
 
 
 class ItemType(db.Model):
     __tablename__ = 'item_type'
     
-    id_ = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(45), nullable=False)
-    model = db.Column(db.String(45), nullable=False)
-    unit_of_measure = db.Column(db.String(45), nullable=False)
+    _id = db.Column(db.Integer, primary_key=True, nullable=False)
+    _name = db.Column(db.String(45), nullable=False)
+    _model = db.Column(db.String(45), nullable=False)
+    _unit_of_measure = db.Column(db.String(45), nullable=False)
 
     def __init__(self, name, model, unit_of_measure):
-        self.name = name
-        self.model = model
-        self.unit_of_measure = unit_of_measure
+        self._name = name
+        self._model = model
+        self._unit_of_measure = unit_of_measure
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return "<ItemType #{}, name: {}>".format(self.id_, self.name)
+        return "<ItemType #{}, name: {}>".format(self._id, self._name)
 
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        self._name = value
+    
+    @property
+    def model(self):
+        return self._model
+    @model.setter
+    def model(self, value):
+        self._model = value
+    
+    @property
+    def unit_of_measure(self):
+        return self._unit_of_measure
+    @unit_of_measure.setter
+    def unit_of_measure(self, value):
+        self._unit_of_measure = value
+    
 
 class Supplier(db.Model):
     __tablename__ = 'supplier'
     
-    id_ = db.Column(db.Integer, primary_key=True, nullable=False)
+    _id = db.Column(db.Integer, primary_key=True, nullable=False)
     # VAT identification number (NIP in Poland)
-    VATIN = db.Column(db.String(45), nullable=False, unique=True)
-    name = db.Column(db.String(45), nullable=False)
-    location = db.Column(db.String(45), nullable=False)
+    _VATIN = db.Column(db.String(45), nullable=False, unique=True)
+    _name = db.Column(db.String(45), nullable=False)
+    _location = db.Column(db.String(45), nullable=False)
 
     def __init__(self, VATIN, name, location):
-        self.VATIN = VATIN
-        self.name = name
-        self.location = location
+        self._VATIN = VATIN
+        self._name = name
+        self._location = location
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return "<Supplier #{}>".format(self.id_)
+        return "<Supplier #{}>".format(self._id)
 
+    @property
+    def VATIN(self):
+        return self._VATIN
+    @VATIN.setter
+    def VATIN(self, value):
+        self._VATIN = value
+    
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        self._name = value    
+
+    @property
+    def location(self):
+        return self._location
+    @location.setter
+    def location(self, value):
+        self._location = value
+    
 
 class ItemBatch(db.Model):
     __tablename__ = 'item_batch'
 
-    id_ = db.Column(db.Integer, primary_key=True, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+    _id = db.Column(db.Integer, primary_key=True, nullable=False)
+    _quantity = db.Column(db.Integer, nullable=False)
    
-    warehouse_id = db.Column(db.Integer, db.ForeignKey("warehouse.id_"),
+    _warehouse_id = db.Column(db.Integer, db.ForeignKey("warehouse._id"),
                               nullable=False)
-    warehouse = db.relationship("Warehouse",
-                                backref=db.backref("item_batches",
+    _warehouse = db.relationship("Warehouse",
+                                 backref=db.backref("item_batches",
                                                    lazy="dynamic"))
     
-    supplier_id = db.Column(db.Integer, db.ForeignKey("supplier.id_"),
+    _supplier_id = db.Column(db.Integer, db.ForeignKey("supplier._id"),
                              nullable=False)
-    supplier = db.relationship("Supplier",
-                               backref=db.backref("item_batches",
+    _supplier = db.relationship("Supplier",
+                                backref=db.backref("item_batches",
                                                   lazy="dynamic"))
 
-    item_type_id = db.Column(db.Integer, db.ForeignKey("item_type.id_"),
-                             nullable=False)
-    item_type = db.relationship("ItemType",
-                                backref=db.backref("item_batches",
+    _item_type_id = db.Column(db.Integer, db.ForeignKey("item_type._id"),
+                              nullable=False)
+    _item_type = db.relationship("ItemType",
+                                 backref=db.backref("item_batches",
                                                    lazy="dynamic"))
 
     def __init__(self, quantity, warehouse, supplier, item_type):
-        self.quantity = quantity
-        self.warehouse = warehouse
-        self.supplier = supplier
-        self.item_type = item_type
+        self._quantity = quantity
+        self._warehouse = warehouse
+        self._supplier = supplier
+        self._item_type = item_type
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return "<ItemBatch #{}>".format(self.id_)
+        return "<ItemBatch #{}>".format(self._id)
+
+    @property
+    def quantity(self):
+        return self._quantity
+    @quantity.setter
+    def quantity(self, value):
+        self._quantity = value
+
+    @property
+    def warehouse_id(self):
+        return self._warehouse_id
+    @warehouse_id.setter
+    def warehouse_id(self, value):
+        self._warehouse_id = value
+
+    @property
+    def supplier_id(self):
+        return self._supplier_id
+    @supplier_id.setter
+    def supplier_id(self, value):
+        self._supplier_id = value
+
+    @property
+    def item_type_id(self):
+        return self._item_type_id
+    @item_type_id.setter
+    def item_type_id(self, value):
+        self._item_type_id = value    
+    
 
 if __name__ == "__main__":
     db.create_all()    
